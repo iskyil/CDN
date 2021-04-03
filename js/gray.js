060d19e5189f5d34f5d8ebf0bec4c18cab00cc8e
Skyilz <74194554+skyilz@users.noreply.github.com>
2021-04-04 01:05:07 +0800

    Delete gray.js

--- js/gray.js
+++
@@ -1,1 +0,0 @@
-function getDate() {return new Date().getMonth()+1+"-"+new Date().getDate()};if(getDate()==="7-7"||getDate()==="9-18"||getDate()==="12-13"||getDate()==="4-4") {document.getElementsByTagName('html')[0].style.filter = "grayscale(1)"}
