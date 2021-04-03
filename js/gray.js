24353b78b099b6381562094c6f730c7180ea961d
Skyilz <74194554+skyilz@users.noreply.github.com>
2021-04-04 00:57:02 +0800

    Create gray.js

---
+++ js/gray.js
@@ -0,0 +1,1 @@
+function getDate() {return new Date().getMonth()+1+"-"+new Date().getDate()};if(getDate()==="7-7"||getDate()==="9-18"||getDate()==="12-13"||getDate()==="4-4") {document.body.style.filter="grayscale(100%)"}
