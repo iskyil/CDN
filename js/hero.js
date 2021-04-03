d5e9c2229dfd61878e4051e59e612cf4d75a1ec5
Skyilz <74194554+skyilz@users.noreply.github.com>
2021-04-04 01:09:56 +0800

    Create hero.js

---
+++ js/hero.js
@@ -0,0 +1,7 @@
+function getDate() { 
+    return new Date().getMonth() + 1 + "-" + new Date().getDate()
+}; 
+if (getDate() === "7-7" || getDate() === "9-18" || getDate() === "12-13" || getDate() === "4-4") {
+    document.getElementsByTagName('html')[0].style.filter = "grayscale(1)" 
+    console.log("Heroes Never Die!!!")
+}
